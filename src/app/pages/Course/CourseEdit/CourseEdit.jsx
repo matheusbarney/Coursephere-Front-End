415f612629363fda2be9854b5c54797a6cd86371
matheusbarney <matheusbarneygalindo@gmail.com>
2026-01-20 17:08:22 -0300

    Refactor styles to use global Tailwind import
    Removed individual page CSS files and consolidated styling by importing Tailwind CSS in src/index.css. Updated references to CSS imports in component files and adjusted index.html to include the global stylesheet.

--- src/app/pages/Course/CourseEdit/CourseEdit.jsx
+++ src/app/pages/Course/CourseEdit/CourseEdit.jsx
@@ -1,5 +1,3 @@
-import '../../../../App.css'
-
 function CourseEdit() {
   return (
     <>
--- src/app/pages/Login/Login.jsx
+++ src/app/pages/Login/Login.jsx
@@ -1,13 +1,37 @@
-
-import '../../../App.css'
+import '../../../index.css'
 
 function Login() {
-  return (
-    <>
 
-      <h1>Login</h1>
-    </>
-  )
+
+  return (
+    <div>
+
+      <h1 className="text-emerald-400 p-8">Login</h1>
+      
+      <div>
+
+        <div>
+          <form>
+          <input type="text" placeholder="Email" />
+          </form>
+        </div>
+
+        <div>
+          <form>
+          <input type="text" placeholder="Password" />
+          </form>
+        </div>
+
+        <div>
+          <button className="p-100">
+            Login to CourseSphere
+          </button>
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
 
 export default Login
