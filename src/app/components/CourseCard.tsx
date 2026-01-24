85a56daecb05ed77ec952c954f2ba0f5ae3a20e6
matheusbarney <matheusbarneygalindo@gmail.com>
2026-01-23 22:15:46 -0300

    feat: Add Lesson List
    Added lesson list in course page and refactored used services with proper typing.

--- src/app/components/CourseCard.tsx
+++ src/app/components/CourseCard.tsx
@@ -3,30 +3,46 @@ import { CourseList } from '../atoms/CourseList';
 import { StandardHeader } from '../atoms/StandardHeader';
 import { LoadingText } from '../atoms/LoadingText';
 import { lessonService } from '../../services/lessonService'
+import { Link } from 'react-router-dom';
 
-
-export function CourseCard({
-  course
-}) {
+export function CourseCard({course}) {
 
   const [lessons, setLessons] = useState(null);  
 
   useEffect(() => {
       const loadLessonsData = async () => {
           try {
-              const data = await lessonService.getAll();
-              setLessons(data);
+            console.log(course);
+            const data = await lessonService.getByCourse(course.id);
+            setLessons(data);
           } catch (error) {
               console.error('Error loading', error);
           }
       };
-      loadLessonsData();
-  }, []);
+      if (course?.id) { loadLessonsData(); }
+  }, [course]);
 
   return <div className="flex-col place-items-center rounded-3xl bg-white px-10 py-15 shadow-xl dark:bg-white/10">
         <StandardHeader text={`Course Details`} />
         <br></br>
         {course ? <CourseList course={course} /> : <LoadingText     />}
+        {lessons ? 
+        lessons.length > 0 ? 
+            <ul>    
+                { lessons.map((c) => <li key={c.id}>
+                    <div className="py-4">
+                        <Link to={`/course/${course.id}/lesson/${c.id}`}>
+                            <p className="font-bold">{c.title}</p>
+                        </Link>
+                        <p>{c.description}</p>
+                    </div>
+                </li>) }
+            </ul>
+            : 
+            <p>No Courses Available.</p>
+        :
+        <LoadingText     />
+        }
       </div>;
 }
   
--- src/services/lessonService.ts
+++ src/services/lessonService.ts
@@ -1,28 +1,43 @@
 import api from "./api";
 
+interface Lesson {
+    id: string | number;
+    course_id: string | number;
+    creator_id: string | number;
+    title: string;
+    status: "draft" | "published" | "archived";
+    publish_date: Date;
+    video_url: string;
+}
+
 export const lessonService = {
     getAll: async () => {
-        const res = await api.get("/lessons");
+        const res = await api.get<Lesson[]>("/lessons");
         return res.data;
     },
 
     getById: async (id) => {
-        const res = await api.get(`/lessons/${id}`);
+        const res = await api.get<Lesson>(`/lessons/${id}`);
+        return res.data;
+    },
+
+    getByCourse: async (course_id) => {
+        const res = await api.get<Lesson[]>(`/lessons?course_id=${course_id}`);
         return res.data;
     },
 
     deleteById: async (id) => {
-        const res = await api.delete(`/lessons/${id}`);
+        const res = await api.delete<void>(`/lessons/${id}`);
         return res.data;
     },
 
     addLesson: async (lessonData) => {
-        const response = await api.post('/lessons', lessonData);
+        const response = await api.post<Lesson>('/lessons', lessonData);
         return response.data;
     },
 
     editLesson: async (id, lessonData) => {
-        const response = await api.put(`/lessons/${id}`, lessonData);
+        const response = await api.put<Lesson>(`/lessons/${id}`, lessonData);
         return response.data;
     },
 
